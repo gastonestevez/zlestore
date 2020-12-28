985ee62a4ce397db206a8e3f5aa8c63107d99b59
tatenalan <taten.usa@gmail.com>
2020-12-28 16:56:16 -0300

    API REST andando

---
+++ app/Http/Controllers/WooCommerceController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Automattic\WooCommerce\Client;
+
+class WooCommerceController extends Controller
+{
+  public function wc()
+  {
+
+    $woocommerce = new Client(
+      'https://zlestore.com',
+      env('WC_KEY_CK'),
+      env('WC_KEY_CS'),
+      [
+        'wp_api' => true,
+        'version' => 'wc/v3',
+      ]
+    );
+
+
+
+    // Seleccionamos, recorremos cada producto y vemos cuanta cantidad piden del producto
+    $cantidad = $woocommerce->get('orders')[0]->line_items[0]->quantity;
+
+    // Si el producto no tiene el campo unidades por caja vacio entonces multiplico el total de cajas por las unidades que lleva dentro
+    if (!empty($woocommerce->get('orders')[0]->line_items[0]->unidades_por_caja)) {
+      $unidades_totales = $cantidad * ($woocommerce->get('orders')[0]->line_items[0]->unidades_por_caja);
+      dd($unidades_totales);
+    } else {
+      dd($cantidad);
+    }
+
+  }
+
+}
