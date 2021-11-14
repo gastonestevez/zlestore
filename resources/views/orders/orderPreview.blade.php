944490f2b034fca914bb6bf00807d66f05792efd
Gastón Estévez <gaston@digitalhouse.edu.ar>
2021-11-13 21:09:29 -0300

    preview order styling

--- resources/views/orders/orderPreview.blade.php
+++ resources/views/orders/orderPreview.blade.php
@@ -11,46 +11,77 @@ ZLE - Confirmar Orden
 <div class="uk-container primer-div">
 
     @php
-       $taxonomies = [];
+        $taxonomies = [];
     @endphp
-    @foreach ($order->orderItems() as $item)
-        ID: {{$item->product_id}} <br>
-        NOMBRE: {{$item->product_name}} <br>
-        SKU: {{$item->product_sku}} <br>
-        PRECIO: ${{ number_format($item->price, 0,',','.')}} <br>
-        CANTIDAD: {{$item->quantity}} <br>
-        @foreach (getProductTaxonomies($item->product_id) as $taxonomy)        
-            {{$taxonomy}} <br>
-            @if (array_search($taxonomy, $taxonomies) === false)
-                @php
-                    $taxonomies[] = $taxonomy
-                @endphp
-            @endif 
-        @endforeach
-        <form action="/removeProduct/{{$item->id}}" method="POST">
-            @method('DELETE')
-            @csrf
-            <button class="uk-button uk-button-default" type="submit">Remover producto</button> <br>
-        </form>
-    @endforeach
+    <h4 class=" uk-heading-line  uk-text-center"> <span>Resumen de orden</span></h4>
+    <table class="uk-table uk-table-divider uk-margin-bottom">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>SKU</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Tags</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($order->orderItems() as $item)
+            <tr>
+                <td>{{$item->product_id}} </td>
+                <td>{{$item->product_name}} </td>
+                <td>{{$item->product_sku}} </td>
+                <td>${{ number_format($item->price, 0,',','.')}} </td>
+                <td>{{$item->quantity}} </td>
+                <td>
+                    @foreach (getProductTaxonomies($item->product_id) as $taxonomy)        
+                        <span class="uk-badge">{{$taxonomy}}</span>
+                        @if (array_search($taxonomy, $taxonomies) === false)
+                            @php
+                                $taxonomies[] = $taxonomy
+                            @endphp
+                        @endif 
+                    @endforeach
+                </td>
+                <td>
+                    <form action="/removeProduct/{{$item->id}}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="uk-button uk-button-default" type="submit">Remover producto</button> <br>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
     {{-- {{dd($taxonomies);}} --}}
-    <br>
-      ORDER TOTAL: ${{ number_format($order->total, 0,',','.')}}
+    <div class="uk-margin-top">
+        <strong>Total:&nbsp;</strong> ${{ number_format($order->total, 0,',','.')}}
+    </div>
 
-    <hr class="uk-divider-icon">
+    <h4 class=" uk-heading-line  uk-text-center uk-margin-top"> <span>Adicionales</span></h4>
 
-    <form action="/orderToPending/{{$id}}" method="POST">
+
+    <form class="uk-form-stacked" action="/orderToPending/{{$id}}" method="POST">
         @method('post')
         @csrf
-        
-        <select name="concept_id" required>
-            <option value="">Seleccione una opción</option>
-            @foreach ($concepts as $concept)
-            <option value="{{$concept->id}}">{{$concept->name}}</option>
-            @endforeach  
-            <option value="2">otros</option>  
-        </select>
-        <a href="/concepts" target="_blank" class="uk-button-link uk-text-lighter">Crear/eliminar/editar conceptos</a>
+        <div class="uk-margin">
+            <div class="uk-form-label">Concepto</div>
+            <div class="uk-form-controls">
+                <select class="uk-select" name="concept_id" required>
+                    <option value="">Seleccione una opción</option>
+                    @foreach ($concepts as $concept)
+                    <option value="{{$concept->id}}">{{$concept->name}}</option>
+                    @endforeach  
+                    <option value="2">otros</option>  
+                </select>
+            </div>
+            <small>
+                <a href="/concepts">Crear/Modificar/Eliminar un concepto</a>
+            </small>
+        </div>
 
         {{-- <select name="" id="">
             <option value="default">Elegi una categoria</option> 
@@ -58,11 +89,16 @@ ZLE - Confirmar Orden
             <option value="{{$taxonomy}}">{{$taxonomy}}</option> 
             @endforeach
         </select> --}}
+        <div class="uk-margin">
+            <div class='uk-form-label'>Información adicional</div>
+            <div class="uk-form-controls">
+                <textarea class="uk-input" name="info" id="" cols="30" rows="10"></textarea>
 
-        <label for="">Información adicional</label>
-        <textarea name="info" id="" cols="30" rows="10"></textarea>
-
-        <button class="uk-button uk-button-default" type="submit">Finalizar orden</button>
+            </div>
+        </div>
+        <div class="uk-margin">
+            <button class="uk-button uk-button-default" type="submit">Finalizar orden</button>
+        </div>
     </form>
 
 </div>
