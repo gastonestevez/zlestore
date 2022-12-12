d1de282ba93d836dd088b92bb9f9516085ac9af6
Tatenalan <taten.usa@gmail.com>
2022-12-12 14:18:47 -0300

    disable backup email notification

--- config/backup.php
+++ config/backup.php
@@ -167,7 +167,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'taten210@gmail.com',
+            'to' => 'your@example.com',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
